c9865a7c9a5b674c49a3fbe9825c9ffe163e334d
Zach P <95437073+picks6@users.noreply.github.com>
2022-05-28 16:33:40 -0400

    fixing webpack issue and updated service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,50 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-
-// Export a function we will use to PUT to the database.
-export const putDb = async (content)  => {
-  console.log('Put to the database');
-
-  // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.put({id: 1, value: content});
-
-  // Get confirmation of the request.
+export const putDb = async (text) => {
+  console.log('Post to the database');
+  const contactDb = await openDB('text', 1);
+  const tx = contactDb.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.add({ text: text });
   const result = await request;
-  console.log('🚀 - data saved to the database', result.value);
+  console.log('🚀 - data saved to the database', result);
 };
-
 // TODO: Add logic for a method that gets all the content from the database
-
 export const getDb = async () => {
-  console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.get(1);
-
-  // Get confirmation of the request.
+  console.log('Get from the database');
+  const contactDb = await openDB('text', 1);
+  const tx = contactDb.transaction('text', 'readonly');
+  const store = tx.objectStore('text');
+  const request = store.getAll();
   const result = await request;
-  result ? console.log('result.value', result.value) :console.log('data not found in database');
-  return result?.value;
+  console.log('result.value', result);
+  return result;
 };
-
 
 initdb();
